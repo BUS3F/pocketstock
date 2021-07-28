f3efe9182c5f90b692dc6cc8866056ecab01c41a
Piaczio <trotroyu@gmail.com>
2021-07-27 19:43:38 -0600

    feat: prelower case strings

--- src/special/uppercases-converter.js
+++ src/special/uppercases-converter.js
@@ -1,12 +1,14 @@
 export function upperConverter(word) {
+ word = word.toLowerCase();
  var array1 = word.split(' ');
  var newarray1 = [];
 
- for (var x = 0; x < array1.length; x++) {
+ for (let x = 0; x < array1.length; x++) {
   newarray1.push(array1[x].charAt(0).toUpperCase() + array1[x].slice(1));
  }
  return newarray1.join(' ');
 }
+
 // export default { upperConverter }
 // Assume str = "the quick brown fox";
 // The split() method is used to split a String object into an array of strings by separating the string into substrings.
